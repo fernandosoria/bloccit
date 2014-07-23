8e13a84815548b57ff57c470c593b8512efff4b6
fernandosoria <soria.fernando@gmail.com>
2014-07-23 21:33:36 +0000

    Using shallow nesting

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,9 @@
 class CommentsController < ApplicationController
   def create
-    @topic = Topic.find(params[:topic_id])
-    @post = @topic.posts.find(params[:post_id])
+    @post = Post.find(params[:post_id])
     @comment = current_user.comments.build(comment_params)
     @comment.post = @post
+    @topic = @post.topic
     authorize @comment
 
     if @comment.save
@@ -16,9 +16,9 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    @topic = Topic.find(params[:topic_id])
-    @post = @topic.posts.find(params[:post_id])
+    @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
+    @topic = @post.topic
     authorize @comment
 
     if @comment.destroy
