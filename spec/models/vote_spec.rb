48243cebc1108e20298138810cca4036cc3c213d
fernandosoria <soria.fernando@gmail.com>
2014-07-25 18:22:31 +0000

    Setup additional testing using Mocks to remove dependencies between classes

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,12 +1,30 @@
 require 'rails_helper'
 
 describe Vote do
+  
+  include TestFactories
+
   describe "validations" do
     describe "value validation" do
       it "only allows -1 or 1 as values" do
-        value = Vote.new(value: 3)
-        expect(value.valid?).to eq(false)
+        v = Vote.new(value: 1)
+        expect(v.valid?).to eq(true)
+        
+        v2 = Vote.new(value: -1)
+        expect(v2.valid?).to eq(true)
+
+        v3 = Vote.new(value: 3)
+        expect(v3.valid?).to eq(false)
       end
+    end
+  end
+
+  describe 'after_save' do
+    it "calls `Post#update_rank` after save" do
+      post = post_without_user
+      vote = Vote.new(value: 1, post: post)
+      expect(post).to receive(:update_rank)
+      vote.save
     end
   end
 end
