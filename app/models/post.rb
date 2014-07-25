2ea10335429172758684b3e06e95e4e3008df59b
fernandosoria <soria.fernando@gmail.com>
2014-07-25 06:11:28 +0000

    Added voting ability for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,15 +2,25 @@ class Post < ActiveRecord::Base
   belongs_to :user
   belongs_to :topic
   has_many :comments, dependent: :destroy
+  has_many :votes, dependent: :destroy
   mount_uploader :image, ImageUploader
 
-
-  default_scope { order('created_at DESC') }
+  default_scope { order('rank DESC') }
 
   validates :title, length: {minimum: 5}, presence: true
   validates :body, length: {minimum: 20}, presence: true
-  validates :topic, presence: true
-  validates :user, presence: true
+
+  def up_votes
+    votes.where(value: 1).count
+  end
+
+  def down_votes
+    votes.where(value: -1).count
+  end
+
+  def points
+    votes.sum(:value)
+  end
 
   def markdown_title
     render_as_markdown title
@@ -20,6 +30,15 @@ class Post < ActiveRecord::Base
     render_as_markdown body
   end
 
+  def update_rank
+    age = (created_at - Time.new(1970,1,1))/(60*60*24) # 1 day in seconds
+    new_rank = points + age
+
+    update_attribute(:rank, new_rank)
+  end
+
+  after_create :create_vote
+
   private
 
   def render_as_markdown(text)    
@@ -27,5 +46,9 @@ class Post < ActiveRecord::Base
     extensions = {fenced_code_blocks: true}
     redcarpet = Redcarpet::Markdown.new(renderer, extensions)
     (redcarpet.render text).html_safe
+  end
+
+  def create_vote
+    user.votes.create(value: 1, post: self)
   end
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,18 +1,11 @@
+require 'rails_helper'
+
 describe Vote do
   describe "validations" do
     describe "value validation" do
       it "only allows -1 or 1 as values" do
-        describe '#up_vote' do
-          it "should equal 1" do
-            expect(up_vote).to eq(1)
-          end
-        end
-        
-        describe '#down_vote' do
-          it "should equal -1" do
-            expect(down_vote).to eq(-1)
-          end
-        end
+        value = Vote.new(value: 3)
+        expect(value.valid?).to eq(false)
       end
     end
   end
