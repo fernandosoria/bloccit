fc53f1486038ecbd66051499fc26a2b8771fd7d5
fernandosoria <soria.fernando@gmail.com>
2014-07-31 20:01:12 +0000

    Updated create method with Ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,15 +5,18 @@ class CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     @comment = current_user.comments.build(comment_params)
     @comment.post = @post
+    @new_comment = Comment.new
     @topic = @post.topic
     authorize @comment
 
     if @comment.save
       flash[:notice] = "Comment was saved"
-      redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error saving the comment. Please try again."
-      redirect_to [@topic, @post]
+    end
+
+    respond_with(@comment) do |format|
+      format.html {respond_to[@topic, @post]}
     end
   end
 
@@ -30,7 +33,7 @@ class CommentsController < ApplicationController
     end
 
     respond_with(@comment) do |format|
-      format.html {redirect_to[@post.topic, @post]}
+      format.html {redirect_to[@topic, @post]}
     end
   end
 
